8f7151adf0df8c5160e996b63c59026a1518c10e
AviIngle <111633187+AviIngle@users.noreply.github.com>
2023-12-05 04:48:33 +0000

    commit

---
+++ React_Project/src/App.jsx
@@ -0,0 +1,25 @@
+
+import './App.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+
+  return (
+    <>
+
+    <h1>Hello, Bootstrap and Vite!</h1>
+
+      <div className="card">
+  <h5 className="card-header">Featured</h5>
+  <div className="card-body">
+    <h5 className="card-title">Special title treatment</h5>
+    <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
+    <a href="/" className="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+
+    </>
+  )
+}
+
+export default App
